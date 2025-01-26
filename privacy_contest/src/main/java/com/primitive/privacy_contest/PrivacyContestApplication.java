a8cf50a51346ca499787c0077afd0d524ae1ddb0
Sejin Choi <67868660+maria2580@users.noreply.github.com>
2025-01-26 16:58:14 +0900

    Add files via upload

---
+++ privacy_contest/src/main/java/com/primitive/privacy_contest/PrivacyContestApplication.java
@@ -0,0 +1,13 @@
+package com.primitive.privacy_contest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacyContestApplication.class, args);
+	}
+
+}
---
+++ privacy_contest/src/test/java/com/primitive/privacy_contest/PrivacyContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primitive.privacy_contest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivacyContestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
