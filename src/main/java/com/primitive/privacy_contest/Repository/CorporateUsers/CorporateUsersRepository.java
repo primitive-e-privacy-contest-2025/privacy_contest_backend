50f25578e1f6076b6e75f2bf7b4897bd84e5faac
maria2580 <marin6670@gmail.com>
2025-02-13 20:28:12 +0900

    findByLoginId생성

--- src/main/java/com/primitive/privacy_contest/Repository/CorporateUsers/CorporateUsersRepository.java
+++ src/main/java/com/primitive/privacy_contest/Repository/CorporateUsers/CorporateUsersRepository.java
@@ -2,6 +2,9 @@ package com.primitive.privacy_contest.Repository.CorporateUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CorporateUsersRepository extends JpaRepository<CorporateUsers, Integer> {
+    List<CorporateUsers> findByLoginId(String loginId);
 }
