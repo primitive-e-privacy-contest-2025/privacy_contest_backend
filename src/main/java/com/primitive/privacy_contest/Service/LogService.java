7474f585fd17b28cde68aac4ce7edf20465d107d
maria2580 <marin6670@gmail.com>
2025-02-19 00:00:26 +0900

    로그 서비스 생성

---
+++ src/main/java/com/primitive/privacy_contest/Service/LogService.java
@@ -0,0 +1,9 @@
+package com.primitive.privacy_contest.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogService {
+    public void createLog(){}
+    public void getLog(){}
+}
