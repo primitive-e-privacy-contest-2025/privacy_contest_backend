f36a526994c4002a001c18bbbf7fec077245b818
maria2580 <marin6670@gmail.com>
2025-02-19 00:04:02 +0900

    유저 가입시 활용되는 가입 dto

---
+++ src/main/java/com/primitive/privacy_contest/DTO/RegistUserDTO.java
@@ -0,0 +1,22 @@
+package com.primitive.privacy_contest.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RegistUserDTO {
+    private String loginId;
+    private String loginPw;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate dateOfBirth;
+    private String address;
+}
