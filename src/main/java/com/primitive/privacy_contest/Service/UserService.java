a230d1541a9d495f9391e7e26b212968a37af27e
maria2580 <marin6670@gmail.com>
2025-02-19 00:03:03 +0900

    유저 가입 구현

--- src/main/java/com/primitive/privacy_contest/Service/UserService.java
+++ src/main/java/com/primitive/privacy_contest/Service/UserService.java
@@ -1,7 +1,10 @@
 package com.primitive.privacy_contest.Service;
 
+import com.primitive.privacy_contest.DTO.RegistUserDTO;
+import com.primitive.privacy_contest.Repository.CorporateUsers.CorporateUsers;
 import com.primitive.privacy_contest.Repository.UserPersonalInfo.UserPersonalInfo;
 import com.primitive.privacy_contest.Repository.UserPersonalInfo.UserPersonalInfoRepository;
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +18,26 @@ public class UserService {
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
-    public UserPersonalInfo updateUser(Long userId, UserPersonalInfo updatedUser) {
+    public long createUser (RegistUserDTO registUserDTO){
+        try {
+            UserPersonalInfo newUser = new UserPersonalInfo();
+            newUser.setLoginId(registUserDTO.getLoginId());
+            newUser.setLoginPw(registUserDTO.getLoginPw());
+            newUser.setFullName(registUserDTO.getFullName());
+            newUser.setEmail(registUserDTO.getEmail());
+            newUser.setPhoneNumber(registUserDTO.getPhoneNumber());
+            newUser.setDateOfBirth(registUserDTO.getDateOfBirth());
+            newUser.setAddress(registUserDTO.getAddress());
+            userRepo.save(newUser);
+            return newUser.getUserId();
+        }catch (Exception e){
+            return (long) -1;
+        }
+
+    }
+
+
+    public UserPersonalInfo updateUser(Long userId, RegistUserDTO updatedUser) {
         UserPersonalInfo user = getUserById(userId);
         user.setFullName(updatedUser.getFullName());
         user.setEmail(updatedUser.getEmail());
